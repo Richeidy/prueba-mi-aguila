e858822bb2623bc0d518247b66e47ad8be41a856
richeidy <richeidy.salcedo@gmail.com>
2022-02-03 18:48:01 -0400

    feat: generated sidebar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SidebarModule } from './sidebar-component/sidebar.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+
+    SidebarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
