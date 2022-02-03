19b678d300991446fc25591bf46590df23ca0b13
richeidy <richeidy.salcedo@gmail.com>
2022-02-03 18:27:03 -0400

    feat: add mapbox

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  mapboxKey:'pk.eyJ1IjoicmljaGVpZHkiLCJhIjoiY2t6N2JlYmppMTh2dDJ0cW8zN3RnY3E2NiJ9.36mQcNMMKvoPpaSguwgRyw'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  mapboxKey:'pk.eyJ1IjoicmljaGVpZHkiLCJhIjoiY2t6N2JlYmppMTh2dDJ0cW8zN3RnY3E2NiJ9.36mQcNMMKvoPpaSguwgRyw'
 };
 
 /*
