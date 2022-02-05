769fc617f22ea1bd7e973d5bebfc89263c9719c6
richeidy <richeidy.salcedo@gmail.com>
2022-02-05 02:46:25 -0400

    feat: created shared pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http'
  
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { SidebarModule } from './sidebar-component/sidebar.module';
+import { SidebarModule } from './sidebar/sidebar.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MapsModule } from './maps/maps.module';
+import { DefaultPageComponent } from './shared/default-page/default-page.component';
+import { ErrorRouteComponent } from './shared/error-route/error-route.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    DefaultPageComponent,
+    ErrorRouteComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/sidebar/sidebar.module.ts
+++ src/app/sidebar/sidebar.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SidebarComponent } from '../sidebar-component/sidebar.component';
+import { SidebarComponent } from '../sidebar/sidebar.component';
 
 import {MatIconModule} from '@angular/material/icon';
 
